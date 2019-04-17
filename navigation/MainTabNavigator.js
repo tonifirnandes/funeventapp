ab0c2f8ddd5ad4546d387fcb39f087341baf91dc
Toni Firnandes <toni@tebengan.id>
2019-04-17 22:26:04 +0700

    base project

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import EventsScreen from '../screens/EventsScreen';
+import UsersScreen from '../screens/UsersScreen';
+import PlacesScreen from '../screens/PlacesScreen';
+
+const HomeStack = createStackNavigator({
+  Home: EventsScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Events',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const LinksStack = createStackNavigator({
+  Links: UsersScreen,
+});
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Users',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: PlacesScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Places',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  LinksStack,
+  SettingsStack,
+});
